45416d1c5a5baf42b9f5b50671f21d7d99c4b77f
yousef samara <92521652+yousefQJ19@users.noreply.github.com>
2023-11-26 17:00:21 +0200

    fix

--- thirdstage-react/src/component/NavBar/navBar.jsx
+++ thirdstage-react/src/component/NavBar/navBar.jsx
@@ -3,11 +3,28 @@ import "./style.scss"
 function NavBar(){
     return(
         <section className="navBar">
-            <p className="navBar__logo"><span>BR</span>Archeticts</p>
+            <p className="navBar__logo">
+                <span>
+                    BR
+                </span>
+                    Archeticts
+            </p>
             <ul className="navBar__list">
-            <li><a href="#Projects">Projects</a></li>
-            <li><a href="#About">About</a></li>
-            <li><a href="#Contact">Contact</a></li>
+            <li>
+                <a href="#Projects">
+                Projects
+                </a>
+            </li>
+            <li>
+                <a href="#About">
+                    About
+                </a>
+            </li>
+            <li>
+                <a href="#Contact">
+                    Contact
+                </a>
+            </li>
             </ul>
         </section>
     )
--- thirdstage-react/src/pages/MainPage.jsx
+++ thirdstage-react/src/pages/MainPage.jsx
@@ -1,16 +1,17 @@
 import NavBar from "../component/NavBar/navBar" ;
-import Header from"../component/Header/header"
-import Projects from "../component/Projects/projects";
-import About from "../component/About/about"
-import Contact from "../component/Contact/contact";
+// import Header from"../component/Header/header"
+// import Projects from "../component/Projects/projects";
+// import About from "../component/About/about"
+// import Contact from "../component/Contact/contact";
 function MainPage(){
         return(
         <div>
                 <NavBar/>
-                <Header/>
+                <h1>iofwmeiofmeoifnm</h1>
+                {/* <Header/>
                 <Projects/>
                 <About/>
-                <Contact/>
+                <Contact/> */}
         </div>
         )
         
