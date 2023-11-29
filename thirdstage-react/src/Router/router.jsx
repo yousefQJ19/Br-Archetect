14eca29be350f74ad9bef8961a73ab4e4f348494
yousef samara <92521652+yousefQJ19@users.noreply.github.com>
2023-11-29 17:40:15 +0200

    fix

--- thirdstage-react/src/Router/router.jsx
+++ thirdstage-react/src/Router/router.jsx
@@ -14,7 +14,6 @@ import About from "../component/About/about";
 const router= createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<RootLayout/>}>
-            
                 <Route index element={<MainPage/>}/>
                 <Route path="Projects" element={<Projects/>}/>
                 <Route path="About" element={<About/>}/>
@@ -26,9 +25,7 @@ const router= createBrowserRouter(
 
 function App(){
     return(
-
         <RouterProvider router={router}/>
-
     )
 }
 
