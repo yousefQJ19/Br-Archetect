3190237fe6e797db5016fb7c66bf3b43904b6e58
yousef samara <92521652+yousefQJ19@users.noreply.github.com>
2023-11-30 09:12:12 +0200

    FIX

--- thirdstage-react/src/component/About/about.jsx
+++ thirdstage-react/src/component/About/about.jsx
@@ -2,10 +2,31 @@ import Card from "../cards/AboutCard/aboutCards"
 import "./aboutStyle.scss"
 
 const description="Phasellus eget enim eu lectus raucibus vestibulum. Suspendisse sodales pellentesque elementum"
-const aboutCards=[{name:"John Doe",position:"CEO & Founder",description:description,imageSource:"7.jpg"},
-             {name:"Jane Doe",position:"Architect",description:description,imageSource:"8.jpg"},
-             {name:"Mike Ross",position:"Architect",description:description,imageSource:"9.jpg"},
-             {name:"Dan Star",position:"Architect",description:description,imageSource:"7.jpg"}]
+const aboutCards=[
+    {
+    name:"John Doe",
+    position:"CEO & Founder",
+    description:description,
+    imageSource:"7.jpg"
+    },
+    {
+    name:"Jane Doe",
+    position:"Architect",
+    description:description,
+    imageSource:"8.jpg"
+    },
+    {
+    name:"Mike Ross",
+    position:"Architect",
+    description:description,
+    imageSource:"9.jpg"
+    },
+    {
+    name:"Dan Star",
+    position:"Architect",
+    description:description,
+    imageSource:"7.jpg"
+    }]
 function About(){
     return(
         <div id="About" 
--- thirdstage-react/src/component/Projects/projects.jsx
+++ thirdstage-react/src/component/Projects/projects.jsx
@@ -2,14 +2,38 @@ import "./style.scss";
 import Card from "../cards/projectsCard/projectsCards";
 
 const housesData = [
-    {houseType:"Summer house",imgSource:"1.jpg"},
-    {houseType:"Bric House",imgSource:"2.jpg"},
-    {houseType:"Renovated",imgSource:"3.jpg"},
-    {houseType:"Barn House",imgSource:"4.jpg"},
-    {houseType:"Summer house",imgSource:"2.jpg"},
-    {houseType:"Bric House",imgSource:"1.jpg"},
-    {houseType:"Renovated",imgSource:"3.jpg"},
-    {houseType:"Barn House",imgSource:"4.jpg"},
+    {
+        houseType:"Summer house",
+        imgSource:"1.jpg"
+    },
+    {
+        houseType:"Bric House",
+        imgSource:"2.jpg"
+    },
+    {
+        houseType:"Renovated",
+        imgSource:"3.jpg"
+    },
+    {
+        houseType:"Barn House",
+        imgSource:"4.jpg"
+    },
+    {
+        houseType:"Summer house",
+        imgSource:"2.jpg"
+    },
+    {
+        houseType:"Bric House",
+        imgSource:"1.jpg"
+    },
+    {
+        houseType:"Renovated",
+        imgSource:"3.jpg"
+    },
+    {
+        houseType:"Barn House",
+        imgSource:"4.jpg"
+    },
 ];
 
 function Projects() {
