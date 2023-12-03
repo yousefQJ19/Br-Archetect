26016ad81cf73adf07a44c89a46b08ddafe102f5
yousef samara <92521652+yousefQJ19@users.noreply.github.com>
2023-12-03 08:58:27 +0200

    formik-yup BR

--- thirdstage-react/src/component/InputField/input.jsx
+++ thirdstage-react/src/component/InputField/input.jsx
@@ -1,16 +1,19 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
+import { ErrorMessage, Field } from "formik";
 import"./inputStyle.scss"
 
-function Input({type,placeHolder}){
+function Input({type,placeHolder,name}){
 
     return(
-        <input type={type}
-               placeholder={placeHolder}
+        <>
+        <Field type={type}
+               name={name}
                className="inputField"
-        />
+               placeholder={placeHolder}/>
+        <ErrorMessage name={name} component="div" />
+        </>
     )
-    
 }
 
 export default Input;
